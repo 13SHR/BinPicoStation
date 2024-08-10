8342f802eb8b74763165bdfe8d4c5b9b5fc3bc4c
Ho-Sun Robin <84474890+13SHR@users.noreply.github.com>
2024-08-10 12:36:37 +0200

    OLED Text setup, beginning of button

--- station.ino
+++ station.ino
@@ -8,6 +8,9 @@ TM1637 TM;
 DHT dht(14, DHT22);
 DisplaySSD1306_128x64_I2C oled(-1);
 
+const int buttonPin = 21;
+int oldValue = LOW; // default/idle value for pin 8 is low.
+
 char strBuf[50];
 
 int colon = 0; // boolean to make the colon blink each second
@@ -35,9 +38,14 @@ void setup() {
   oled.setFixedFont( ssd1306xled_font6x8 );
   oled.begin();
   oled.clear();
+
+  // Input
+  pinMode(buttonPin, INPUT);
 }
 
 void loop() {
+  int newValue = digitalRead(buttonPin);
+
   DateTime now = rtc.now();
   int hour = now.hour();
   int minutes = now.minute();
@@ -45,16 +53,22 @@ void loop() {
   TM.displayTime(hour, minutes, colon); // Display time
   colon = colon ? 0 : 1;
 
-  float t = dht.readTemperature();
-  float h = dht.readHumidity();
-  Serial1.print(t);
-  Serial1.print(" / ");
-  Serial1.print(h);
-  Serial1.println();
-
-  oled.printFixed(0,  8, "Station Sensors", STYLE_NORMAL);
-  oled.printFixed(0,  16, "Placeholder Temperature", STYLE_NORMAL);
-  oled.printFixed(0,  24, "Placeholder Humidity", STYLE_NORMAL);
+  if (newValue != oldValue) {
+    if (newValue == HIGH) {
+      float t = dht.readTemperature();
+      float h = dht.readHumidity();
+      
+      oled.clear();
+      oled.printFixed(24,  8, "Station Sensors", STYLE_NORMAL);
+      sprintf(strBuf, "Temp: %.2f C", t);
+      oled.printFixed(0,  24, strBuf, STYLE_NORMAL);
+      sprintf(strBuf, "Hum: %.2f %%", h);
+      oled.printFixed(0,  32, strBuf, STYLE_NORMAL);
+    }
+    else {
+      oled.clear();
+    }
+  }
   
   delay(1000);
 }
